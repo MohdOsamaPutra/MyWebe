d0b462be0427c516c6d1b3bd12b95b979b9994c2
belajarPweb04-2023 <12150114394@students.uin-suska.ac.id>
2023-05-21 22:58:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mywebe/kagura.java
@@ -0,0 +1,14 @@
+package com.example.mywebe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kagura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kagura);
+    }
+}
